36254903358edc5810cf6ba3260760df44904a1f
Kevin Lamping <klamping@gmail.com>
2015-03-11 22:51:25 -0500

    add in autoprefixer; move styles to .scss file; fix safari bug;

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var autoprefixer = require('gulp-autoprefixer');
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -14,8 +15,12 @@ var paths = {
 gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
+  gulp.src('./scss/*.scss')
     .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(gulp.dest('./www/css/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
