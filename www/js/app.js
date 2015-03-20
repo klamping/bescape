9311747eef8b3a435be5e4e5cd9be587b6fa685d
Kevin Lamping <klamping@gmail.com>
2015-03-19 23:21:18 -0500

    rewrite of prompts; rename to busymind

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('bescape', ['ionic', 'ngCordova'])
+angular.module('busymind', ['ionic', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,173 +15,159 @@ angular.module('bescape', ['ionic', 'ngCordova'])
 
 .value('slides', [
   {
-    prompt: '<h2>Bescape</h2><p>Mindfulness for your busy life</p>',
+    prompt: '<h2>Busymind</h2><p>Mindfulness for your busy life</p>',
     buttons: [{
       action: 'advanceSlide()',
-      text: 'Start your Session',
+      text: 'Begin your Session',
       type: 'calm'
     }]
   },
   {
-    prompt: '<p>Take a moment and feel your heart beat in your chest.</p><p>Is it racing or is it calm?</p>',
+    prompt: '<p>Start by focusing on your breath.</p><p>Feel your chest expand and contract. Feel the air move in and out.</p><p><strong>Take five deep breaths, holding the button as you breath in, and releasing it as you breath out.</strong></p>',
     buttons: [{
-        type: 'assertive',
-        text: 'It\'s Racing',
-        action: 'advanceSlide()'
+      type: 'calm breath',
+      text: 'I\'m Breathing',
+      action: 'countThenAdvance(5)'
+    }]
+  },
+  {
+    prompt: '<p>Now sense your heart beating in your chest.</p><p>Feel its steady rhythm.</p><p>Anticipate each beat, trying to slow the cadence of your heart.</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m Slowing Down',
+      action: 'advanceSlide()'
+    }]
+  },
+  {
+    prompt: '<p>Search your body for tension.</p><p>This could be at your shoulders, back, or even pursed lips.</p><p>Be aware of this tension.</p><p>Choose a tense spot. Tighten it, then relax fully to relieve the stress.</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m relaxing',
+      action: 'advanceSlide()'
+    }]
+  },
+  {
+    prompt: '<p>Find where on your body feels most relaxed right now.</p><p>Focus on that area, relaxing as deeply as you can.</p><p>Sink in to the full weight of your relaxation, letting gravity pull down.<p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m letting go',
+      action: 'advanceSlide()'
+    }]
+  },
+  {
+    prompt: '<p>Turn outward. Open your mind to the sounds around you.</p><p>What\'s the texture of the noise. Is it loud or quiet?</p><p><strong>Hold the button for 15 seconds while you listen to your world.</strong></p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m Listening',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
+    }]
+  },
+  {
+    prompt: '<p>Look around you.</p><p>Notice what colors and patterns are hiding in plain sight.</p><p>Hold the button for another 15 seconds while you look closer.</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m Seeing',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
+    }]
+  },
+  {
+    prompt: '<p>Draw back inward.</p><p>Observe your internal dialogue.</p><p>Let thoughts rise up.</p><p>Take 15 seconds to let your mind wander.</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m wandering',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
+    }]
+  },
+  {
+    prompt: '<p>Come back to now. Did you notice any fear or anxiety in your thoughts?</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'Yes',
+      action: 'advanceSlide()'
     }, {
-        type: 'calm',
-        text: 'It\'s Calm',
-        action: 'advanceSlide(2)'
+      type: 'positive',
+      text: 'No',
+      action: 'advanceSlide(2)'
     }]
   },
   {
-    prompt: '<p>Take a few extra moments to slow down.</p><p>Take a deep breath and settle in, letting your racing heart calm.</p>',
+    prompt: '<p>Your emotions are neither good nor bad, they simply are.</p><p>Think of how you would objectively describe the emotion to someone.</p><p>Hold for 15 seconds as you talk it out.</p>',
     buttons: [{
-        type: 'positive',
-        text: 'I\'m slowing down.',
-        action: 'advanceSlide()'
+      type: 'calm',
+      text: 'I\'m Explaining',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
     }]
   },
   {
-    prompt: '<p>Now focus on your breathing.</p><p>Draw in six deep breaths. Feel yourself breathing in and out.</p><p><strong>Hold the button as you breath in, and release it as you breath out.</strong></p>',
+    prompt: '<p>Having observed your emotion non-judgementally, can you transform it?</p><p>Can you radically accept it?</p><p>Don\'t worry, you don\'t have to have find an answer right now. Just process.</p><p>Hold the button for 15 seconds while you think.</p>',
     buttons: [{
-        type: 'calm breath',
-        text: 'I\'m Breathing.',
-        action: 'countThenAdvance(6)'
+      type: 'calm',
+      text: 'I\'m Thinking',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
     }]
   },
   {
-    prompt: '<p>Notice the places in your body where there is tension or pain.</p><p>This could be at your shoulders, core, back, or even your pursed lips.</p><p>Tighten completely, then let go of your tension with your awareness of it.</p>',
+    prompt: '<p>Focus on your breathing once more.</p><p>Draw in five deep breaths.</p><p>Hold the button while breathing in, and release it as you breath out.</p>',
     buttons: [{
-        type: 'assertive',
-        text: 'I\'m still tense.',
-        action: 'advanceSlide()'
+      type: 'calm breath',
+      text: 'I\'m Breathing',
+      action: 'countThenAdvance(5)'
+    }]
+  },
+  {
+    prompt: '<p>Rewind your day. What has brought you happiness?</p><p>Can you find that happiness again?</p><p>Hold the button for 15 seconds while you consider it.</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'I\'m Finding Happiness',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
+    }]
+  },
+  {
+    prompt: '<p>Happiness can also bring fear.</p><p>As humans, we accept both the strength and the weakness as eqaul parts.</p><p>Did your happiness stir any fear or anxiety?</p>',
+    buttons: [{
+      type: 'calm',
+      text: 'Yes',
+      action: 'advanceSlide()'
     }, {
-        type: 'calm',
-        text: 'I\'m feeling relaxed.',
-        action: 'advanceSlide(2)'
+      type: 'positive',
+      text: 'No',
+      action: 'advanceSlide(2)'
     }]
   },
   {
-    prompt: '<p>That\'s okay. Life can be stressful. We don\'t always have time to relax.</p><p>Notice the places in your body where you do feel relaxed.</p><p>See if you can enhance your relaxation in those place.</p>',
+    prompt: '<p>Think about those fears and play them out.</p><p>Will they truly prevent you from obtaining your happiness and aspirations?</p><p>Hold the button for 15 seconds and unfold your anxiety.</p>',
     buttons: [{
-        type: 'calm',
-        text: 'I\'m relaxing.',
-        action: 'advanceSlide()'
+      type: 'calm',
+      text: 'I\'m Accepting Fear',
+      hold: 'waitThenAdvance(15)',
+      release: 'cancelAdvance()'
     }]
   },
   {
-    prompt: '<p>Listen to the sounds around you. Are they loud, soft, or in-between?</p><p><strong>Hold the button for 15 seconds while you listen to your world.</strong></p>',
+    prompt: '<p>Almost done. Focus on your breathing one last time.</p><p>Press the button down, take one deep breath, let go and return to the world.</p>',
     buttons: [{
-        type: 'calm',
-        text: 'I\'m Listening',
-        hold: 'waitThenAdvance(15)',
-        release: 'cancelAdvance()'
+      type: 'calm breath',
+      text: 'I am Complete',
+      action: 'advanceSlide()'
     }]
   },
   {
-    prompt: '<p>Look around you. Notice any interesting colors or patterns?</p><p>Hold the button for another 15 seconds while you experience the sights.</p><!-- <p>It will vibrate when complete.</p> -->',
+    prompt: '<p>This is you, alive and well.</p><p>When ready, return to your day in your own time.</p>',
     buttons: [{
-        type: 'calm',
-        text: 'I\'m Seeing',
-        hold: 'waitThenAdvance(15)',
-        release: 'cancelAdvance()'
-    }]
-  },
-  {
-    prompt: '<p>Time for your thoughts.</p><p>As you listen, focus on being non-judgemental.</p><p>Each thought is neither good nor bad, it just is.</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'I\'m non-judgemental.',
-        action: 'advanceSlide()'
-    }]
-  },
-  {
-    prompt: '<p>Take a moment on each thought and accept it.</p><p>After a moment, let it go and allow another to rise up.</p><p>Hold the button for each thought, releasing when your done. Do this five times.</p>',
-    buttons: [{
-        type: 'calm breath',
-        text: 'I\'m Processing.',
-        action: 'countThenAdvance(5)'
-    }]
-  },
-  {
-    prompt: '<p>Did you have any fear or anxiety with these thoughts?</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'Yes',
-        action: 'advanceSlide()'
-    }, {
-        type: 'positive',
-        text: 'No',
-        action: 'advanceSlide(2)'
-    }]
-  },
-  {
-    prompt: '<p>Can you transform that fear?</p><p>Can you radically accept it?</p><p>Don\'t worry, you don\'t have to have find an answer right now. Just process.</p><p>Hold the button for 15 seconds while you think.</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'I\'m Transforming',
-        hold: 'waitThenAdvance(15)',
-        release: 'cancelAdvance()'
-    }]
-  },
-  {
-    prompt: '<p>Focus on your breathing once more.</p><p>Draw in six deep breaths.</p><p>Hold the button while breathing in, and release it as you breath out.</p>',
-    buttons: [{
-        type: 'calm breath',
-        text: 'I\'m Breathing.',
-        action: 'countThenAdvance(6)'
-    }]
-  },
-  {
-    prompt: '<p>Think to yourself, what is your happiness today?</p><p>Once you find it, consider how you can maintain it.</p><p>Hold the button for 15 seconds while you think.</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'I\'m Finding Happiness.',
-        hold: 'waitThenAdvance(15)',
-        release: 'cancelAdvance()'
-    }]
-  },
-  {
-    prompt: '<p>Happiness can also bring fear.</p><p>As humans, we accept both the strength and the weakness as eqaul parts.</p><p>Did you have any fear or anxiety with these thoughts?</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'Yes',
-        action: 'advanceSlide()'
-    }, {
-        type: 'positive',
-        text: 'No',
-        action: 'advanceSlide(2)'
-    }]
-  },
-  {
-    prompt: '<p>Think about your fear. Will your fear prevent you from maintaining your happiness and aspirations?</p><p>Hold the button for 15 seconds and think.</p>',
-    buttons: [{
-        type: 'calm',
-        text: 'I\'m Accepting Fear.',
-        hold: 'waitThenAdvance(15)',
-        release: 'cancelAdvance()'
-    }]
-  },
-  {
-    prompt: '<p>Focus on your breathing one last time.</p><p>Press the button down, take one deep breath, let go and return to the world.</p>',
-    buttons: [{
-        type: 'calm breath',
-        text: 'I am Complete.',
-        action: 'advanceSlide()'
-    }]
-  },
-  {
-    prompt: '<p>This is you, alive and breathing.</p><p>When ready, return to your day in your own time.</p>',
-    buttons: [{
-        type: 'calm breath',
-        text: 'Restart Session.',
-        action: 'restartSlides()'
+      type: 'calm breath',
+      text: 'End Session',
+      action: 'restartSlides()'
     }]
   }
 ])
 
-.controller('bescapeCtrl', function($scope, $interval, $cordovaVibration, $ionicPlatform, slides, $timeout) {
+.controller('busymindCtrl', function($scope, $interval, $cordovaVibration, $ionicPlatform, slides, $timeout) {
   var waiter;
   $scope.activeSlide = 0;
   $scope.slides = slides;
@@ -223,9 +204,9 @@ angular.module('bescape', ['ionic', 'ngCordova'])
     if ($scope.count === total) {
       $scope.advanceSlide();
       $timeout(function () {
-        $scope.countTotal = 0;
-        $scope.count = 0;
-        line.set(0);
+      $scope.countTotal = 0;
+      $scope.count = 0;
+      line.set(0);
       }, 250);
     }
   };
@@ -247,24 +228,24 @@ angular.module('bescape', ['ionic', 'ngCordova'])
       $scope.count += 1;
 
       seconds.animate($scope.count / duration, {
-          duration: 200
-      }, function() {
-        seconds.setText($scope.count);
-      });
+        duration: 200
+    }, function() {
+      seconds.setText($scope.count);
+    });
 
       if ($scope.count > duration) {
-        $scope.waiting = false;
+      $scope.waiting = false;
 
-        $scope.total = 0;
+      $scope.total = 0;
         $scope.count = 0;
         seconds.set(0);
         seconds.setText('');
 
         // $ionicPlatform.ready(function() {
-        //   if ($cordovaVibration) {
-        //     $cordovaVibration.vibrate(100);
-        //   }
-        // });
+      //   if ($cordovaVibration) {
+      //     $cordovaVibration.vibrate(100);
+      //   }
+      // });
 
         $interval.cancel(waiter);
         $scope.advanceSlide(distance);
@@ -281,34 +262,3 @@ angular.module('bescape', ['ionic', 'ngCordova'])
     $interval.cancel(waiter);
   };
 });
-
-
-
-  // {
-  //   prompt: '<p>If possible, silence your phone</p>',
-  //   buttons: [{
-  //     action: 'advanceSlide()',
-  //     text: 'Okay',
-  //     type: 'balanced'
-  //   }]
-  // },
-  // {
-  //   prompt: '<p>Are you in a calm environment?</p>',
-  //   buttons: [{
-  //       type: 'calm',
-  //       text: 'Yes',
-  //       action: 'advanceSlide(2)'
-  //   }, {
-  //       type: 'royal',
-  //       text: 'No',
-  //       action: 'advanceSlide()'
-  //   }]
-  // },
-  // {
-  //   prompt: '<p>That\'s okay. Life is hectic sometimes. It\'s why this app exists.</p><p>If possible, try going for a walk to avoid interruptions, or escape to a solitary room.</p>',
-  //   buttons: [{
-  //       type: 'royal',
-  //       text: 'Okay',
-  //       action: 'advanceSlide()'
-  //   }]
-  // },
